5f266e893d86670e601645e990f6c812bd89ea85
Sachin Shah <sachin_shah@redmane.com>
2022-06-11 09:39:59 -0500

    Initial commit

---
+++ template/fsharp-httprequest/Program.fs
@@ -0,0 +1,28 @@
+namespace fsharp_httprequest
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+module Program =
+    let createHostBuilder args =
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(fun webBuilder ->
+                webBuilder
+                    .Configure(Configuration.configureApp)
+                    .ConfigureServices(Configuration.configureServices)
+                    .ConfigureLogging(Configuration.configureLogging)
+                |> ignore)
+
+    [<EntryPoint>]
+    let main args =
+        createHostBuilder(args).Build().Run()
+
+        0 // Exit code

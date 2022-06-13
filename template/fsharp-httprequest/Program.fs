8d8cb6ecc446921b77ba7047e5cecabf5db0e8c5
zakaluka <zakthrow@gmail.com>
2022-06-13 08:17:58 -0500

    follow giraffe exapmles
    Signed-off-by: zakaluka <znprojects.apps@gmail.com>

--- template/fsharp-httprequest/Program.fs
+++ template/fsharp-httprequest/Program.fs
@@ -1,4 +1,4 @@
-namespace fsharp_httprequest
+﻿namespace fsharp_httprequest
 
 open System
 open System.Collections.Generic
@@ -10,19 +10,63 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Giraffe
+open Microsoft.Extensions.Logging
 
 module Program =
-    let createHostBuilder args =
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder
-                    .Configure(Configuration.configureApp)
-                    .ConfigureServices(Configuration.configureServices)
-                    .ConfigureLogging(Configuration.configureLogging)
-                |> ignore)
+  let errorHandler (ex: Exception) (logger: ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
 
-    [<EntryPoint>]
-    let main args =
-        createHostBuilder(args).Build().Run()
+    clearResponse
+    >=> setStatusCode 500
+    >=> text ex.Message
 
-        0 // Exit code
+  let webApp =
+    choose [ route "/" >=> Successful.OK "get hola que tal" 
+             RequestErrors.notFound <| text "Not Found" ]
+
+  let configureApp (app: IApplicationBuilder) =
+    app
+      .UseGiraffeErrorHandler(errorHandler)
+      .UseStaticFiles()
+      .UseResponseCaching()
+      .UseGiraffe webApp
+
+  let configureServices (svc: IServiceCollection) =
+    svc.AddResponseCaching().AddGiraffe() |> ignore
+    svc.AddDataProtection() |> ignore
+
+  let configureLogging (loggerBuilder: ILoggingBuilder) =
+    loggerBuilder
+      .AddFilter(fun lvl -> lvl.Equals LogLevel.Error)
+      .AddConsole()
+      .AddDebug()
+    |> ignore
+
+  let createHostBuilder args =
+    Host
+      .CreateDefaultBuilder(args)
+      .ConfigureWebHostDefaults(fun webBuilder ->
+        webBuilder
+          .Configure(configureApp)
+          .ConfigureServices(configureServices)
+          .ConfigureLogging(configureLogging)
+          .UseUrls([| "http://localhost:5000" |])
+        |> ignore)
+
+  [<EntryPoint>]
+  let main args =
+    printfn "Starting server..."
+    let host = createHostBuilder(args).Build()
+
+    host.Run()
+    printfn "Server running..."
+
+
+    0 // Exit code
